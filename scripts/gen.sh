cc160cd158027e6a9ee1eac5d152c362ecc359d8
hayao <shun819.mail@gmail.com>
2025-09-15 13:12:10 +0900

    feat: Refactor voice generation script to utilize character definitions from voices.json

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -2,49 +2,86 @@
 
 set -eEuo pipefail
 
-output_dir="${1-""}"
-if [[ -z "$output_dir" ]]; then
-	echo "Usage: $0 <output directory>"
-	exit 1
-fi
-
 script_dir=$(cd "$(dirname "${0}")" && pwd)
 export GMV_CORE_DIR="${script_dir}/../core"
 
+target_charactor=""
+output_dir=""
+voices_json_path="$script_dir/../data/voices.json"
+voices=()
+charactors=()
+
 # shellcheck source=/dev/null
 source "$GMV_CORE_DIR/voicevox.sh"
-# shellcheck source=/dev/null
-source "$GMV_CORE_DIR/kiritan.sh"
-# shellcheck source=/dev/null
-source "$GMV_CORE_DIR/ttchan.sh"
 
-voices_json_path="$script_dir/../data/voices.json"
-
-readarray -t voices < <(jq -c '.voices.base[]' "$voices_json_path")
-
-for voice in "${voices[@]}"; do
-	filename=$(echo "$voice" | jq -r '.filename')
-	id=$(echo "$voice" | jq -r '.id')
-	text=$(echo "$voice" | jq -r '.text')
-
-	if [[ -z "$text" ]]; then
-		echo "Skipping id $id (no text)"
-		continue
+function init() {
+	target_charactor="${1:-}"
+	output_dir="${2:-}"
+	if [[ -z "$output_dir" ]] || [[ -z "$target_charactor" ]]; then
+		echo "Usage: $0 <voice> <output directory>" >&2
+		return 1
 	fi
 
-	{
-		output_path="$output_dir/kiritan/${filename}.wav"
-		mkdir -p "$(dirname "$output_path")"
+	readarray -t voices < <(jq -c '.voices.base[]' "$voices_json_path")
+	readarray -t charactors < <(jq -c '.charactors[]' "$voices_json_path")
 
-		echo "Generating id $id: $text (kiritan)"
-		kiritan "$text" "$output_path"
-	} 
-	{
-		output_path="$output_dir/ttchan/${filename}.wav"
-		mkdir -p "$(dirname "$output_path")"
+	mkdir -p "$output_dir"
+}
 
-		echo "Generating id $id: $text (ttchan)"
-		ttchan "$text" "$output_path"
-	} 
-done
-wait
+# util: select_charactor "charactor_name" -> json
+function select_charactor() {
+	local _setected_json
+	_selected_json="$(printf "%s\n" "${charactors[@]}" | jq "select(.name == \"$1\")")"
+	if [[ -z "$_selected_json" ]]; then
+		echo "Error: charactor $1 not found" >&2
+		return 1
+	fi
+	echo "$_selected_json"
+}
+
+# util: call_charactor "charactor_name" "args..."
+function call_charactor() {
+	local _selected_json
+	_selected_json="$(select_charactor "$1")" || return 1
+	shift
+
+	local _source _command
+	_source=$(jq -r '.source' <<<"$_selected_json")
+	_command=$(jq -r '.command' <<<"$_selected_json")
+	if [[ -z "$_source" ]] || [[ -z "$_command" ]]; then
+		echo "Error: charactor $1 has no source or command" >&2
+		return 1
+	fi
+
+	# shellcheck source=/dev/null
+	source "$GMV_CORE_DIR/$_source"
+
+	"$_command" "$@"
+}
+
+function main() {
+	local _charactor_source
+
+	local _v
+	for _v in "${voices[@]}"; do
+		local _id _text _filename
+		_id=$(jq -r '.id' <<<"$_v")
+		_text=$(jq -r '.text' <<<"$_v")
+		_filename=$(jq -r '.filename' <<<"$_v")
+
+		if [[ -z "$_text" ]]; then
+			echo "Skipping id $_id (no text)"
+			continue
+		fi
+
+		local _output_path="$output_dir/${_filename}.wav"
+
+		call_charactor "$target_charactor" "$_text" "$_output_path" --speed-scale 1.1 || {
+			echo "Error: failed to generate id $_id" >&2
+			exit 1
+		}
+		echo "Generated id $_id: $_text -> $_output_path"
+	done
+}
+
+init "$@" && main
